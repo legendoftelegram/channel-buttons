e2d6f908675408ca95d591f72837036de6fbd683
anandus <49789123+anandus0070@users.noreply.github.com>
2020-04-20 04:23:14 -0700

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -63,7 +63,6 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT,
-        reply_to_message_id=update.message_id
     )
    
 
