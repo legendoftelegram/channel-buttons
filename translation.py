1d0f33ae392f073d01968192f5cf915ef9558b18
anandus <49789123+anandus0070@users.noreply.github.com>
2020-04-20 04:38:18 -0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,9 @@
 class Translation(object):
-    START_TEXT = """safe,fast,stable"""
+    START_MSG = """`made by @TR_TORRENTS for accepting DMCA requests from public users or respective owners`
+   <b>👉 നിങ്ങളുടേത് എന്ന് അവകാശപ്പെടുന്ന ഫയൽ അഥവാ കോപ്പിറയിട്ടെഡ് മറ്റിരിയൽ ഇൗ ബോടിലേക്ക്‌ ഫോർവേർഡ് ചെയ്യുക നിങ്ങളുടെ യൂസർ നെയിമും ഇൗ ബോടിലെക് അയക്കുക
+Files വേഗം തന്നെ രിമൂവ് ആകുന്നതായിരികും 
+👉Forward the files to this bot that you own. may we infringed we will remove it pls sent ur username or id incase forward is locked in ur settings
+we will remove the files soon as possible</b>""" 
     NOT_AUTH_USER_TEXT = "You are banned"
     NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File SizenPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/TheUseLessGroup'>@SpEcHlDe</a>"
     HELP_USER = """ANY ISSUES CONTACT @LEGENDOFTELEGRAM"""
