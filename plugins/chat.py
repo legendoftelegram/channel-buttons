79d1ccf0101a0313c0388020b6f4319d264d63a9
anandus <49789123+anandus0070@users.noreply.github.com>
2020-04-20 04:51:36 -0700

    Update chat.py

--- plugins/chat.py
+++ plugins/chat.py
@@ -61,15 +61,11 @@ async def start(bot, update):
    
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
-async def upgrade(bot, update):
-    # logger.info(update)
+@pyrogram.Client.on_message(pyrogram.Filters.text & ~Filters.edited)
+async def chats(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.UPGRADE_TEXT,
-        parse_mode="html",
-        reply_to_message_id=update.message_id,
-        disable_web_page_preview=True
+        text=Translation.SUC_SE,
     )
     
     
