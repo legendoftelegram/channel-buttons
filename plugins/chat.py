128b9939d47718841044dc0c4535450c31cab360
anandus <49789123+anandus0070@users.noreply.github.com>
2020-04-20 21:59:43 -0700

    Update chat.py

--- plugins/chat.py
+++ plugins/chat.py
@@ -73,7 +73,7 @@ async def chats(bot, update):
         from_chat_id=update.chat.id,
         message_ids=update.message_id
     )
-    forward_from=rep
+    rep=forward_from
     await bot.send_message(
         chat_id=rep.chat.id,
         from_chat_id=update.chat.id,
