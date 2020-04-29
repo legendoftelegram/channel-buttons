6b8f814778002532ca88ada363e21b0a1f6ac36b
king legend <49789123+anandus0070@users.noreply.github.com>
2020-04-29 02:59:36 -0700

    Update chat.py

--- plugins/chat.py
+++ plugins/chat.py
@@ -67,6 +67,7 @@ async def chats(bot, update):
     await bot.edit_message_text(
         chat_id=update.chat.id,
         message_id=x.message_id,
+        text=update.text,
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("🥴SUPPORT🥴",
             url="https://t.me/joinhereforcross")]]
