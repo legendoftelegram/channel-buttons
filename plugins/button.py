c6ea926647043328b1794f458d1629ac4458a62a
king legend <49789123+anandus0070@users.noreply.github.com>
2020-04-30 05:45:04 -0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -51,7 +51,7 @@ async def chats(bot, update):
         chat_id=int("-1001242101409"),
         message_id=update.message_id,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("🔥shɑre & support🔥",
+            [[InlineKeyboardButton("🧿old malayalam movies🧿",
             url="https://t.me/share/url?url=https://t.me/joinchat/AAAAAEoI9qHQDl54X6hrnA")]] 
         )
     )
