e23c83fbfb8b87fee569b6af80715b4ff3ffea0f
king legend <49789123+anandus0070@users.noreply.github.com>
2020-04-30 04:19:49 -0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,16 +1,9 @@
 class Translation(object):
-    START_MSG = """`made by @TR_TORRENTS for accepting DMCA requests from public users or respective owners`
-   <b>👉 നിങ്ങളുടേത് എന്ന് അവകാശപ്പെടുന്ന ഫയൽ അഥവാ കോപ്പിറയിട്ടെഡ് മറ്റിരിയൽ ഇൗ ബോടിലേക്ക്‌ ഫോർവേർഡ് ചെയ്യുക നിങ്ങളുടെ യൂസർ നെയിമും ഇൗ ബോടിലെക് അയക്കുക
-Files വേഗം തന്നെ രിമൂവ് ആകുന്നതായിരികും 
-👉Forward the files to this bot that you own. may we infringed we will remove it pls sent ur username or id incase forward is locked in ur settings
-we will remove the files soon as possible</b>""" 
+    START_MSG = """@TR_TORRENTS `channel helper`
+   <b>made by @legendoftelegram</b>""" 
     HELP = """
-👉 <b>ബോട്ടിലേക്ക്‌ എന്ത് മെസ്സേജ് അയച്ചാലും ചാനൽ അഡ്മിൻസീന് ഫോർവേർഡ് ആകും</b>
-
-👉<b>Every message sent to this bot automatically forwarded to channel admins</b>
-      
+👉 <b>ask dev</b>
 <b>Bot developed by</b> @Legendoftelegram"""
-    SUC_SE = """`we have successfully recieved your request` <b>if you forgotten to sent your usename or id sent it now for further updates☺️ and have a nice day enjoy🤘</b>"""
     CH_LIST = """<b>we own the following channels</b>
   👉@ZMOVIES10
      @TR_TORRENTS
