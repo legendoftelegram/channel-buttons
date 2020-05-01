597e6f34a6187856b7cbe8cc755b3b4165af4172
king legend <49789123+anandus0070@users.noreply.github.com>
2020-04-30 22:26:59 -0700

    Create hevc.py

---
+++ plugins/hevc.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+from pyrogram import Client,Filters, ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP
+    )
+  
+  
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_MSG,
+    )
+   
+
+@pyrogram.Client.on_message(pyrogram.Filters.video | Filters.document)
+async def hevc(bot, update):
+    await bot.edit_message_reply_markup(
+        chat_id=int("-1001419345563"),
+        message_id=update.message_id,
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🌀HEVC DOWNLOAD🌀",
+            url="https://t.me/joinchat/AAAAAFSZfpvuqvHrlJ-Vig")]] 
+        )
+    )   
