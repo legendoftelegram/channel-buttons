fb48c9b1265a485bd3962e9de2b0a1e0d89b2cdf
king legend <49789123+anandus0070@users.noreply.github.com>
2020-05-04 04:43:46 -0700

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,17 +1,7 @@
 class Translation(object):
-    START_MSG = """@TR_TORRENTS `channel helper`
+    START_MSG = """`channel button helper`
    <b>made by @legendoftelegram</b>""" 
     HELP = """
 👉 <b>ask dev</b>
 <b>Bot developed by</b> @Legendoftelegram"""
-    CH_LIST = """<b>we own the following channels</b>
-  👉@ZMOVIES10
-     @TR_TORRENTS
-     @QualityCinemasHD
-     @Malayalam_Movies_Z
-     @Malayalam_Old_Movies_Z
-     @HOLLYWOODKANIBEL
-     @ZMOVIES400MB
-     @ZMOVIESALL
-
-  <b>🧿join 🧿shɑre 🧿support</b>"""  
+    CH_LIST = """<b>empty</b>"""  
