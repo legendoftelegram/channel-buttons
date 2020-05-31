c97f59e0cf5c7df3c6f000e4d4d200f80ac65f12
king legend <49789123+anandus0070@users.noreply.github.com>
2020-05-30 20:52:11 -0700

    Update old.py

--- plugins/old.py
+++ plugins/old.py
@@ -53,10 +53,10 @@ async def old(bot, message):
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton('🌀TᴀᴍɪʟRᴏᴄᴋᴇʀs★🌀', url='https://t.me/joinchat/AAAAAEoI9qHQDl54X6hrnA')],
-                [InlineKeyboardButton("🌀HEVC🌀", url="https://t.me/joinchat/AAAAAFSZfpvuqvHrlJ-Vig"), InlineKeyboardButton("🌀OLD movies🌀", url="https://t.me/joinchat/AAAAAFMMxf1wotQy7G3kfg")],
-                [InlineKeyboardButton("🌀Malayalam🌀", url="https://t.me/joinchat/AAAAAFPCFsHvCo9WTClaVg"), InlineKeyboardButton("🌀English🌀", url="https://t.me/joinchat/AAAAAFcgVJN1SCE_QDcLRg")],
+                [InlineKeyboardButton("🌀HEVC🌀", url="https://t.me/joinchat/AAAAAFSZfpvuqvHrlJ-Vig"), InlineKeyboardButton("🌀OLD movies🌀", url="https://t.me/joinchat/AAAAAFMMxf2ymyV1UfUMBw")],
+                [InlineKeyboardButton("🌀Malayalam🌀", url="https://t.me/joinchat/AAAAAFPCFsFEnq6eI7tSJQ"), InlineKeyboardButton("🌀English🌀", url="https://t.me/joinchat/AAAAAFcgVJN1SCE_QDcLRg")],
                 [InlineKeyboardButton("🌀Kannada🌀", url="https://t.me/joinchat/AAAAAFco7KkVwmdDvF8LJw"), InlineKeyboardButton("🌀WEB SERIES🌀", url="https://t.me/joinchat/AAAAAEXHnHCKUuSUu0yM2A")],
-                [InlineKeyboardButton("🌀All movies🌀", url="https://t.me/joinchat/AAAAAESroNzOW8OJKX4hCg"), InlineKeyboardButton("🌀400MB🌀", url="https://t.me/joinchat/AAAAAEL_N1cxaMN4GGEctw")],
+                [InlineKeyboardButton("🌀All movies🌀", url="https://t.me/joinchat/AAAAAESroNxVmruuhxs7KA"), InlineKeyboardButton("🌀400MB🌀", url="https://t.me/joinchat/AAAAAEL_N1cxaMN4GGEctw")],
                 [InlineKeyboardButton('🌀TR NETWORK🌀', url='https://t.me/TR_NETWORK')],
             ]
         )
