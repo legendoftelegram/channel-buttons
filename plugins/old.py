44668c5275ea1aa5795702dcf8e8bd49c5aca67c
king legend <49789123+anandus0070@users.noreply.github.com>
2020-06-10 06:37:45 -0700

    Update old.py

--- plugins/old.py
+++ plugins/old.py
@@ -56,7 +56,6 @@ async def old(client, message):
                     [InlineKeyboardButton('🌀TᴀᴍɪʟRᴏᴄᴋᴇʀs★🌀', url=Config.TAMIL_LINK)],
                     [InlineKeyboardButton("🌀HEVC🌀", url=Config.HEVC_LINK), InlineKeyboardButton("🌀OLD movies🌀", url=Config.OLD_LINK)],
                     [InlineKeyboardButton("🌀Malayalam🌀", url=Config.MALA_LINK), InlineKeyboardButton("🌀English🌀", url=Config.ENGLISH_LINK)],
-                    [InlineKeyboardButton('🌀TR NETWORK🌀', url='https://t.me/TR_NETWORK')],
                 ]
             )
         )
