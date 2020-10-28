e17c469ebad4778fd5e1be762a7069460b9f1f15
king legend <49789123+legendoftelegram@users.noreply.github.com>
2020-10-28 17:35:59 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
     TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
-    API_ID = int(os.environ.get("APP_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     
