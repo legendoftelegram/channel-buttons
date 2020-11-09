4f243934fc5fb96f738808a524a56770220c35c3
king legend <49789123+legendoftelegram@users.noreply.github.com>
2020-11-09 07:55:29 +0530

    Update old.py

--- plugins/old.py
+++ plugins/old.py
@@ -24,7 +24,7 @@ async def start(bot, update):
    
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.document)
+@pyrogram.Client.on_message(pyrogram.Filters.document) # @pyrogram.Client.on_message(pyrogram.Filters.document | Filters.video) set like this to trigger both or remove filters.document and add filters.video for video only
 async def old(client, message):
     await client.edit_message_reply_markup(
         chat_id=message.chat.id,
